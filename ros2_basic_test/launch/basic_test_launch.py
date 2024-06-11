4d7e0f6d7758311bd5a0b0868ba54b3d76e2f4b4
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-06-12 03:28:30 +0900

    Update basic_test_launch.py

--- ros2_basic_test/launch/basic_test_launch.py
+++ ros2_basic_test/launch/basic_test_launch.py
@@ -1,1 +1,15 @@
 # ros2_basic_test
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+    
+        Node(
+            package='ros2_basic_test', 
+            executable='rostopic_pub', 
+            output='screen',
+            emulate_tty=True),
+      
+    ])
