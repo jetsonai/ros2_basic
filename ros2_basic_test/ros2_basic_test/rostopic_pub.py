570a68682638f9f060bc85f08bd930cd900fd391
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-06-12 03:30:32 +0900

    Update rostopic_pub.py

--- ros2_basic_test/ros2_basic_test/rostopic_pub.py
+++ ros2_basic_test/ros2_basic_test/rostopic_pub.py
@@ -14,20 +14,19 @@ class BasicTopicPublisher(Node):
     def __init__(self):
         super().__init__('rostopic_pub')
         qos_profile = QoSProfile(depth=10)
-        self.counter_publisher = self.create_publisher(String, 'counter', qos_profile)
-        self.timer = self.create_timer(1, self.publish_counter_msg)
+        #TODO
         self.count = 0
 
     def publish_counter_msg(self):
         msg = String()
         msg.data = 'Hello ROS2: {0}'.format(self.count)
-        self.counter_publisher.publish(msg)
+        #TODO
         self.get_logger().info('Published message: {0}'.format(msg.data))
         self.count += 1
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    #TODO
     node = BasicTopicPublisher()
     try:
         rclpy.spin(node)
